5641a541409842fa5f3f2b0aa786436c2705cff1
Felix.Price <zhupengfei1995@gmail.com>
2023-12-12 21:24:00 +0800

    feat: 保留最精简的目录结构

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import { FC } from 'react'
-import Demo from './Demo'
-import data from './const.json'
-import styles from './styles.module.less'
-import styless from './styles.less'
+import styles from './styles.less'
 
 type AppType = {
 	count: number
 }
 
 const App: FC<AppType> = () => {
-	const arr = ['11', '22', '333']
-	console.log(data)
-	console.log(styless)
 	return (
 		<div
 			id='xxx'
@@ -20,11 +14,7 @@ const App: FC<AppType> = () => {
 			style={{ width: 200 }}
 		>
 			<p>ddddd</p>
-			{arr.map((item) => {
-				return <span key={item}>{item}</span>
-			})}
-			<Demo />
-			<div className={styless.myComponent}>11111111</div>
+			<div className={styles.myComponent}>11111111</div>
 		</div>
 	)
 }
